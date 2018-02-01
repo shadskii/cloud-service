a2a961c07fd6c7b99f7caca34f0e9a8a31879f94
Jake <Jake.k.hassel@gmail.com>
2018-02-01 00:24:46 -0500

    Unused imports

--- lines-data/src/main/java/com/github/shadskii/data/LineRepository.java
+++ lines-data/src/main/java/com/github/shadskii/data/LineRepository.java
@@ -1,6 +1,5 @@
 package com.github.shadskii.data;
 
-import com.github.shadskii.data.Line;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface LineRepository extends MongoRepository<Line, String>
