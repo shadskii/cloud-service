56bb7fa8cd0e161a1e6391460fa010e3e1bf6fe4
shadskii <jake.k.hassel@gmail.com>
2018-02-02 01:06:08 -0500

    Server no longer implements commandlinerunner

--- lines-service/src/main/java/com/github/shadskii/Server.java
+++ lines-service/src/main/java/com/github/shadskii/Server.java
@@ -1,11 +1,10 @@
 package com.github.shadskii;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Server implements CommandLineRunner
+public class Server
 {
 
     public static void main(String[] args)
@@ -16,9 +15,4 @@ public class Server implements CommandLineRunner
         SpringApplication.run(Server.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception
-    {
-
-    }
 }
