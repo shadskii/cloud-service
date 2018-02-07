aaa6094c56133ec779417637af872a63532b8b42
Jake <Jake.k.hassel@gmail.com>
2018-02-06 20:20:52 -0500

    Added documentation

--- lines-generator/src/main/java/com/github/shadskii/FileUtils.java
+++ lines-generator/src/main/java/com/github/shadskii/FileUtils.java
@@ -6,9 +6,16 @@ public class FileUtils
 {
     public static void main(String[] args) throws IOException
     {
-        generateTestFile("testFile_10_000.txt", 10_000);
+        generateTestFile("testFile_10_000.txt", 100_000_000);
     }
 
+    /**
+     * Generates a simple file that can be used for test purposes. Each line in the file is in the form: "This line is
+     * line [line number].
+     *
+     * @param filePath Name of the file to create
+     * @param numLines Number of lines to write in the file.
+     */
     public static void generateTestFile(String filePath, long numLines)
     {
         try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filePath))))
