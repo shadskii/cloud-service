d881aba014d9a66559edf5b966fa91b807fd05d5
Jake <jake.k.hassel@gmail.com>
2018-02-12 19:59:13 -0500

    Install haproxy from build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # Bash script to build the project and run unit tests
 ./gradlew clean build
+sudo add-apt-repository ppa:vbernat/haproxy-1.7
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo apt-get install -y haproxy
